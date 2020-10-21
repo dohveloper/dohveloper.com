485d2cc7898d2943ab4f1d8f9ca30d65a7198645
dohveloper <dohveloper@gmail.com>
2020-10-21 16:26:13 -0400

    error fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,:recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable,  :trackable and :omniauthable
+  devise :database_authenticatable,:recoverable, :rememberable,:registerable,:validatable
 end
